619f2a05caa7ca89da503a8e8a44553d620a3701
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-02-17 16:15:41 +0300

    basic data types

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::env::var;
 
 fn main() {
     let mut x = 5;
@@ -11,5 +10,24 @@ fn main() {
     //shadowing
     let var1 = "test";
     let var1 = var1.len();
-    println!("var1 = {var1}")
+    println!("var1 = {var1}");
+
+
+    //truples
+    let turp: (i32, f32, i128) = (200, 28.23, 732832);
+    let (x, y , z) = turp;
+    println!("Turp is {x}, {y}, {z}");
+
+    //arrays
+    let a = [1,2,5,7];
+    let b: [i32; 3] = [10,20,30];
+
+    //initialise to contain 8 elements of the same value for each element 
+    //same as let a = [3,3,3,3,3,3,3]
+    let c = [3;8]; 
+
+    //accessing
+    let first = a[2]; //5
+    
+
 }
