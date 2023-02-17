86e782e87662e646bdf8b1e2bf1d70d8e33044d6
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-02-17 14:53:59 +0300

    variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env::var;
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    const AMOUNT:u32 = 234;
+    println!("Value of x: {x}");
+    x = 12;
+    println!("Value of x: {x}");
+    println!("Value of AMOUNT: {AMOUNT}");
+
+    //shadowing
+    let var1 = "test";
+    let var1 = var1.len();
+    println!("var1 = {var1}")
 }
