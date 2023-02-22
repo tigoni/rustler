5f56e31f33c4d1e9775b30704bbeb67a29dc0e0b
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-02-22 16:44:42 +0300

    structs methods

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 
 
 
-use crate::structures::testStruct::User;
+use crate::structures::testStruct::{User, Book, Point, Liquid};
 
 mod structures;
 fn main() {
@@ -14,15 +14,50 @@ let mut user = User{
     };
 
     user.email = String::from("another.emai.co");
+
+
     //a builder function to update the field
     fn build_user(email: String, username: String) -> User {
         User {
             active: true,
-            username: username,
-            email: email,
+            username,
+            email,
         }
     }
     println!("Email is: {:?}", build_user(String::from("someemail.com"), String::from("anotherusername")));
+
+    //using struct update syntax
+    let mut book1 = Book {
+        title: String::from("Guards Guards!"),
+        author: String::from("Terry Pratchett"),
+        price: 1250,
+    };
+    let mut book2 = Book {
+        title: String::from("12 Rules for Life"),
+        author: String::from("Jordan Peterson"),
+        price: 1800,
+    };
+
+    //update book 2 using struct update syntax (like js spread syntax)
+    let mut book3 = Book {
+        title: String::from("Maps of Meaning"),
+        ..book2 //this should be last 
+    };
+
+    println!("Book 3 description: {:?}", book3);
+
+    //turple structs - No names associated with the fields, just types
+    //Can be descructrured into individual pieces and the . operator can be 
+    //used followed by the index tp accesss each value
+    let point1 = Point(2,8);
+    println!("{:?}", point1);
+    println!("Y axix point = {:?}", point1.0);
+
+    let liquid = Liquid;
+
+    //using methods
+    let discounted_price = book1.calc_discount();
+    println!("The price of book1 after duscount: {discounted_price}");
 
 }
 
--- src/structures.rs
+++ src/structures.rs
@@ -6,4 +6,37 @@ pub mod testStruct {
         pub username: String,
         pub email: String,
     }
+
+
+    #[derive(Debug)]
+    pub struct Book {
+        pub title: String,
+        pub author: String,
+        pub price: i32
+    }
+
+    //turple struct: No field names, just types.
+    pub struct Color(pub i32, pub i32, pub i32);
+
+    #[derive(Debug)]
+    pub struct Point(pub i32, pub i32);
+
+    //unit-like structs: No fields! 
+    //useful for implementing traits on data types but no data to put in
+    pub struct Liquid;
+
+
+    //Method-Syntax: functions defined within the context of a struct
+    //The first param is always self which represents the instance of the struct the 
+    //function is being called on.
+
+    //definition starts with an implementation block. Everything inside this block will be associated 
+    //with Book type
+    impl Book {
+        pub fn calc_discount(&self) -> i32 {
+            self.price * 2
+        }
+
+    }
+
 }
